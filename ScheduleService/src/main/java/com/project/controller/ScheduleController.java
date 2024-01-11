ca1231feac7306bea5c3a23b097d19d0f5a2f327
Leon <doncorleone142857@gmail.com>
2024-01-11 11:36:34 +0700

    CORS for schedule controller

--- ScheduleService/src/main/java/com/project/controller/ScheduleController.java
+++ ScheduleService/src/main/java/com/project/controller/ScheduleController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/v1/schedule")
 public class ScheduleController {
 
